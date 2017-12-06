3a3b9740b71d27d8c051e83ba1bd12510f218a9c
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-06 19:05:05 +0100

    Add files via upload

---
+++ ConsoleApplication48/ConsoleApplication48/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte liczba1 = 30;
+            byte liczba2 = 0;
+            byte liczba3 = (byte)(liczba1 / liczba2);
+        }
+    }
+}
